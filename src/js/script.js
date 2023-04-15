ab366fd01f5638f0dbbfa7eeb5135babad99187d
HIROKI <hiroki@codeups.jp>
2023-04-15 16:45:17 +0900

    途中

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,27 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
-
+  //メインビューのスライダー実装内容
+    var swiper = new Swiper(".js-mv-swiper", {
+      effect: "fade",
+      loop: true,
+      speed: 1000,
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false
+      }
+    });
+  //制作実績のスライダー実装内容
+    var swiper = new Swiper(".js-works-swiper", {
+      loop: true,
+      speed: 1000,
+      pagination: {
+        el: ".js-works-pagination",
+        clickable: true
+      },
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false
+      },
+    });
 
 });
