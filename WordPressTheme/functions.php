331a669c5aed30b5135e2307197252af0866a98d
HIROKI-MATSUMURA <hiroki@codeups.jp>
2023-06-25 13:15:13 +0900

    breakpointsとfunctions.phpの修正

--- WordPressTheme/functions.php
+++ WordPressTheme/functions.php
@@ -5,23 +5,25 @@
 
 /**
  * WordPress標準機能
- */
-/**
- * カスタムテーマ機能設定
+ *
+ * @codex https://wpdocs.osdn.jp/%E9%96%A2%E6%95%B0%E3%83%AA%E3%83%95%E3%82%A1%E3%83%AC%E3%83%B3%E3%82%B9/add_theme_support
  */
 function my_setup() {
-    // アイキャッチ画像、RSSフィード、タイトルタグのサポート
-    add_theme_support('post-thumbnails', 'automatic-feed-links', 'title-tag');
-
-    // HTML5タグでの出力サポート
-    add_theme_support('html5', [
-        'search-form', 'comment-form', 'comment-list', 'gallery', 'caption'
-    ]);
+	add_theme_support( 'post-thumbnails' ); /* アイキャッチ */
+	add_theme_support( 'automatic-feed-links' ); /* RSSフィード */
+	add_theme_support( 'title-tag' ); /* タイトルタグ自動生成 */
+	add_theme_support(
+		'html5',
+		array( /* HTML5のタグで出力 */
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+		)
+	);
 }
-
-// テーマ設定を初期化
-add_action('after_setup_theme', 'my_setup');
-
+add_action( 'after_setup_theme', 'my_setup' );
 
 
 
@@ -45,75 +47,105 @@ add_action('wp_enqueue_scripts', 'my_script_init');
 
 /**
  * メニューの登録
+ *
+ * @codex https://wpdocs.osdn.jp/%E9%96%A2%E6%95%B0%E3%83%AA%E3%83%95%E3%82%A1%E3%83%AC%E3%83%B3%E3%82%B9/register_nav_menus
  */
-function my_menu_init() {
-	register_nav_menus(
-		array(
-			'global'  => 'ヘッダーメニュー',
-			'utility' => 'ユーティリティメニュー',
-			'drawer'  => 'ドロワーメニュー',
-		)
-	);
-}
-add_action( 'init', 'my_menu_init' );
+// function my_menu_init() {
+// 	register_nav_menus(
+// 		array(
+// 			'global'  => 'ヘッダーメニュー',
+// 			'utility' => 'ユーティリティメニュー',
+// 			'drawer'  => 'ドロワーメニュー',
+// 		)
+// 	);
+// }
+// add_action( 'init', 'my_menu_init' );
+/**
+ * メニューの登録
+ *
+ * 参考：https://wpdocs.osdn.jp/%E9%96%A2%E6%95%B0%E3%83%AA%E3%83%95%E3%82%A1%E3%83%AC%E3%83%B3%E3%82%B9/register_nav_menus
+ */
 
 
 /**
  * ウィジェットの登録
+ *
+ * @codex http://wpdocs.osdn.jp/%E9%96%A2%E6%95%B0%E3%83%AA%E3%83%95%E3%82%A1%E3%83%AC%E3%83%B3%E3%82%B9/register_sidebar
  */
-function my_widget_init() {
-	register_sidebar(
-		array(
-			'name'          => 'サイドバー',
-			'id'            => 'sidebar',
-			'before_widget' => '<div id="%1$s" class="p-widget %2$s">',
-			'after_widget'  => '</div>',
-			'before_title'  => '<div class="p-widget__title">',
-			'after_title'   => '</div>',
-		)
-	);
-}
-add_action( 'widgets_init', 'my_widget_init' );
+// function my_widget_init() {
+// 	register_sidebar(
+// 		array(
+// 			'name'          => 'サイドバー',
+// 			'id'            => 'sidebar',
+// 			'before_widget' => '<div id="%1$s" class="p-widget %2$s">',
+// 			'after_widget'  => '</div>',
+// 			'before_title'  => '<div class="p-widget__title">',
+// 			'after_title'   => '</div>',
+// 		)
+// 	);
+// }
+// add_action( 'widgets_init', 'my_widget_init' );
 
 
 /**
  * アーカイブタイトル書き換え
+ *
+ * @param string $title 書き換え前のタイトル.
+ * @return string $title 書き換え後のタイトル.
  */
-function my_archive_title($title) {
-    if (is_home()) {
-        $title = 'ブログ';
-    } elseif (is_category() || is_tag() || is_post_type_archive() || is_tax()) {
-        $title = single_term_title('', false);
-    } elseif (is_search()) {
-        $title = '「' . esc_html(get_query_var('s')) . '」の検索結果';
-    } elseif (is_author()) {
-        $title = get_the_author();
-    } elseif (is_date()) {
-        $title = '';
-        if ($year = get_query_var('year')) {
-            $title .= $year . '年';
-        }
-        if ($month = get_query_var('monthnum')) {
-            $title .= $month . '月';
-        }
-        if ($day = get_query_var('day')) {
-            $title .= $day . '日';
-        }
-    }
-    return $title;
+function my_archive_title( $title ) {
+
+	if ( is_home() ) { /* ホームの場合 */
+		$title = 'ブログ';
+	} elseif ( is_category() ) { /* カテゴリーアーカイブの場合 */
+		$title = '' . single_cat_title( '', false ) . '';
+	} elseif ( is_tag() ) { /* タグアーカイブの場合 */
+		$title = '' . single_tag_title( '', false ) . '';
+	} elseif ( is_post_type_archive() ) { /* 投稿タイプのアーカイブの場合 */
+		$title = '' . post_type_archive_title( '', false ) . '';
+	} elseif ( is_tax() ) { /* タームアーカイブの場合 */
+		$title = '' . single_term_title( '', false );
+	} elseif ( is_search() ) { /* 検索結果アーカイブの場合 */
+		$title = '「' . esc_html( get_query_var( 's' ) ) . '」の検索結果';
+	} elseif ( is_author() ) { /* 作者アーカイブの場合 */
+		$title = '' . get_the_author() . '';
+	} elseif ( is_date() ) { /* 日付アーカイブの場合 */
+		$title = '';
+		if ( get_query_var( 'year' ) ) {
+			$title .= get_query_var( 'year' ) . '年';
+		}
+		if ( get_query_var( 'monthnum' ) ) {
+			$title .= get_query_var( 'monthnum' ) . '月';
+		}
+		if ( get_query_var( 'day' ) ) {
+			$title .= get_query_var( 'day' ) . '日';
+		}
+	}
+	return $title;
+};
+add_filter( 'get_the_archive_title', 'my_archive_title' );
+
+
+/**
+ * 抜粋文の文字数の変更
+ *
+ * @param int $length 変更前の文字数.
+ * @return int $length 変更後の文字数.
+ */
+function my_excerpt_length( $length ) {
+	return 80;
 }
-add_filter('get_the_archive_title', 'my_archive_title');
+add_filter( 'excerpt_length', 'my_excerpt_length', 999 );
 
 
+/**
+ * 抜粋文の省略記法の変更
+ *
+ * @param string $more 変更前の省略記法.
+ * @return string $more 変更後の省略記法.
+ */
+function my_excerpt_more( $more ) {
+	return '...';
 
-// 抜粋文の文字数を変更する関数
-function my_excerpt_length($length) {
-    return 80;    // 80文字に設定
 }
-add_filter('excerpt_length', 'my_excerpt_length', 999); // 'excerpt_length'フィルターに関数を適用
-
-// 抜粋文の省略記法を変更する関数
-function my_excerpt_more($more) {
-    return '...'; // 省略記法を'...'に変更
-}
-add_filter('excerpt_more', 'my_excerpt_more'); // 'excerpt_more'フィルターに関数を適用
+add_filter( 'excerpt_more', 'my_excerpt_more' );
